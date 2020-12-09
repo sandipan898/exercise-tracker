2996e6363e75935ef5170bc15f515bd772d0d7a3
sandipan <sandipan.das898@gmail.com>
2020-12-10 02:46:08 +0530

    updating create-user exit-exercise create-exercise exercise-list component

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors()); 
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
--- frontend/src/components/create-user.component.js
+++ frontend/src/components/create-user.component.js
@@ -28,9 +28,9 @@ export default class CreateUser extends Component {
 
         console.log(user);
 
-        axios.post('http://localhost:500/users/add', user)
+        axios.post('http://localhost:5000/users/add', user)
         .then(res => console.log(res.data));
-        
+
         this.setState({
             username: ''
         })
