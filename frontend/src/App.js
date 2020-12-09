17e8647c42a729139886fa6ad79317467779dfca
sandipan <sandipan@localhost.localdomain>
2020-12-09 10:03:25 +0530

    updating app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import React from "react";
-import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
-import Navbar from "./components/navbar.component";
-import ExerciseList from "./components/exercise-list.component";
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercise-list.component";
 import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <br/>
-      <Route path="/" exact component={ExerciseList} />
-      <Route path="/edit/:id" exact component={EditExercise} />
-      <Route path="/create" exact component={CreateExercise} />
-      <Route path="/user" exact component={CreateUser} />
-    </Router>
-  );
+    return (
+        <Router>
+          <div className="container">
+            <Navbar />
+            <br/>
+            <Route path="/" exact component={ExercisesList} />
+            <Route path="/edit/:id" component={EditExercise} />
+            <Route path="/create" component={CreateExercise} />
+            <Route path="/user" component={CreateUser} />
+          </div>
+        </Router>
+    );
 }
 
 export default App;
